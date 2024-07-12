b8f1582a1242c3177752b5d9d58a4a3ab266104f
raphael-co <76869389+raphael-co@users.noreply.github.com>
2024-07-12 16:30:45 +0200

    test on serveur

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -10,4 +10,8 @@ routes.use('/upload', uploads);
 routes.use('/files', downloads);
 routes.use('/stats', stats);
 
+routes.get('/test', (req, res) => {
+    res.status(200).json({ message: 'is runnig' });
+});
+
 export default routes;
